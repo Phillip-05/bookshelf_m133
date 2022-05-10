a80efa4911f2f8c9899086a24e986cb32f8e12a5
Phillip <jerebicp@bzz.ch>
2022-05-10 15:07:04 +0200

    ...

---
+++ src/main/java/ch/bzz/bookshelf/model/Book.java
@@ -0,0 +1,60 @@
+package ch.bzz.bookshelf.model;
+
+import java.math.BigDecimal;
+
+public class Book {
+    private String bookUUID;
+    private String title;
+    private String author;
+    private String isbn;
+    private Publisher publisher;
+    private BigDecimal price;
+
+    public String getBookUUID() {
+        return bookUUID;
+    }
+
+    public void setBookUUID(String bookUUID) {
+        this.bookUUID = bookUUID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
